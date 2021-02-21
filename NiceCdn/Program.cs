4d9d18321da854c61ac063cbb78193390f096d5d
FF <FFantasy@live.cn>
2021-02-21 17:02:23 +0800

    update

--- NiceCdn/Program.cs
+++ NiceCdn/Program.cs
@@ -12,7 +12,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace PingTest
+namespace NiceCdn
 {
     class Config {
         public string CfWork { get; set; }
